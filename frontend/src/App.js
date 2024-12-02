543bfbc4ac535693e7cfe4b40615984978c327da
cryptovaibhav1 <cryptovaibhav1@gmail.com>
2024-12-02 12:39:41 +0530

    removed commented code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,9 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { OktoProvider, BuildType } from 'okto-sdk-react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
-import Home from "./components/Home";
-import RawTxnPage from "./components/RawTxnPage";
-import WidgetPage from "./components/WidgetPage";
 import Main from "./components/Main";
-
 
 const OKTO_CLIENT_API_KEY = process.env.REACT_APP_OKTO_CLIENT_API_KEY;
 
@@ -20,12 +15,6 @@ function App() {
      console.log("setting auth token to null")
      setAuthToken(null); // Clear the authToken
   };
-
-  // React.useEffect(() => {
-  //   fetch("/api")
-  //     .then((res) => res.json())
-  //     .then((data) => setData(data.message));
-  // }, []);
 
   return (
     <Router>
